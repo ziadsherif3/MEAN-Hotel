e6c99479a33dbc7e9d3b9212532bca0a3dfe3882
ziadsherif3 <ziadsherif3@gmail.com>
2020-10-17 18:08:07 +0200

    Imported express and sat up the port

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("Start command by npm.")
+const express = require('express');
+const app = express(); // The top-level function of the express application.
+
+app.set('port', 3000);
+
+const server = app.listen(app.get('port'), () => {
+    const port = server.address().port;
+    console.log(`This is port ${port}`);
+});
