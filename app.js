765c0337ff065587ca716efd01b180b5789c0b41
ziadsherif3 <ziadsherif3@gmail.com>
2020-10-17 20:15:14 +0200

    Added GET responses to some requests

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const app = express(); // The top-level function of the express application.
+const path = require('path');
 
 app.set('port', 3000);
+
+app.get('/', (req, res) => {
+    console.log(`GET request to homepage is made.`);
+    res.status(200).sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/json', (req, res) => {
+    console.log(`GET request to json is made.`);
+    res.status(200).json({ "name": "Ziad", "age": 22 });
+});
+
+app.get('/file', (req, res) => {
+    console.log(`GET request to file is made.`);
+    res.status(200).sendFile(path.join(__dirname, ''));
+});
 
 const server = app.listen(app.get('port'), () => {
     const port = server.address().port;
