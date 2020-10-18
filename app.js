ddc38733c2d9c82811604610d772622917a34eb5
ziadsherif3 <ziadsherif3@gmail.com>
2020-10-18 21:06:37 +0200

    Added static resources, a css theme, and a middleware to log the requests

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const path = require('path');
 
 app.set('port', 3000);
 
-app.get('/', (req, res) => {
-    console.log(`GET request to homepage is made.`);
-    res.status(200).sendFile(path.join(__dirname, 'public', 'index.html'));
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
 });
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/json', (req, res) => {
     console.log(`GET request to json is made.`);
@@ -16,7 +18,7 @@ app.get('/json', (req, res) => {
 
 app.get('/file', (req, res) => {
     console.log(`GET request to file is made.`);
-    res.status(200).sendFile(path.join(__dirname, ''));
+    res.status(200).sendFile(path.join(__dirname, 'app.js'));
 });
 
 const server = app.listen(app.get('port'), () => {
