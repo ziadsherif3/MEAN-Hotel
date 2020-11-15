2364a355f819470f058b1d45a18859f2d07d64d9
ziadsherif3 <ziadsherif3@gmail.com>
2020-11-15 20:01:34 +0200

    Replaced the custom directive with a custom component

--- public/angular-app/hotelRating.js
+++ public/angular-app/hotelRating.js
@@ -1,14 +1,23 @@
-angular.module('hotel').directive('hotelRating', hotelRating);
+// angular.module('hotel').directive('hotelRating', hotelRating);
 
-function hotelRating() {
-    return {
-        restrict: 'E',
-        scope: {},
-        bindToController: {
-            stars: '='
-        },
-        controller: 'HotelController',
-        controllerAs: 'vm',
-        template: '<span ng-repeat="star in vm.stars track by $index">{{ star }}</span>'
-    }
-}
+// function hotelRating() {
+//     return {
+//         restrict: 'E',
+//         scope: {},
+//         bindToController: {
+//             stars: '='
+//         },
+//         controller: 'HotelController',
+//         controllerAs: 'vm',
+//         template: '<span ng-repeat="star in vm.stars track by $index">{{ star }}</span>'
+//     }
+// }
+
+angular.module('hotel').component('hotelRating', {
+    bindings: {
+        stars: '='
+    },
+    template: '<span ng-repeat="star in vm.stars track by $index" class="fa fa-star checked""></span>',
+    controller: 'HotelController',
+    controllerAs: 'vm'
+});
